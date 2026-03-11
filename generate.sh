2b9543b23c410001b60c4a6119940d2781bb3cb1
Mystia-Izakaya <duwwwy@gmail.com>
2026-03-11 21:44:57 +0300

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -6,7 +6,8 @@ COMPILER="./compiler/dart-sass/sass"
 
 # Check if ImageMagick is installed
 if [ ! "$(which magick)" ]; then
-	echo "You don't have ImageMagick installed. Aborting..."
+	echo "You don't have ImageMagick installed."
+	echo "Aborting..."
 	exit 1
 fi
 
@@ -26,13 +27,15 @@ if [ ! -d ./SlickCold ] && [ ! -d ./SlickFire ]; then
 	cp -r "./src" "./SlickCold"
 	cp -r "./src" "./SlickFire"
 else
+	echo "Stale directories detected, you must do manual cleanup."
+	echo "Aborting..."
 	exit 1
 fi
 
 # Transform SlickCold to SlickFire
 echo "Transforming SlickCold..."
-find ./SlickFire -regex ".*\.png\|.*\.jpg" -type f -print0 | xargs -0 -I{} magick {} -channel-fx "blue, red, red" "{}.out"
-find ./SlickFire -regex ".*\.png\|.*\.jpg" -type f -print0 | xargs -0 -I{} mv "{}.out" "{}"
+find "./SlickFire" -regex ".*\.png\|.*\.jpg" -type f -print0 | xargs -0 -I{} magick {} -channel-fx "blue, red, red" "{}.out"
+find "./SlickFire" -regex ".*\.png\|.*\.jpg" -type f -print0 | xargs -0 -I{} mv "{}.out" "{}"
 sed -i "s/#\([0-9A-Fa-f]\{2\}\)\([0-9A-Fa-f]\{2\}\)\([0-9A-Fa-f]\{2\}\)/#\3\1\1/g" "./SlickFire/gtk-3.0/internal/_gradients.scss"
 sed -i "s/#\([0-9A-Fa-f]\{2\}\)\([0-9A-Fa-f]\{2\}\)\([0-9A-Fa-f]\{2\}\)/#\3\1\1/g" "./SlickFire/gtk-2.0/gtkrc"
 sed -i "s/SlickCold/SlickFire/g" "./SlickFire/index.theme" "./SlickFire/metacity-1/metacity-theme-1.xml" "./SlickFire/metacity-1/metacity-theme-3.xml"
@@ -47,13 +50,13 @@ echo "Compiling Sass..."
 
 # Clean up unneeded files
 echo "Cleaning up..."
-rm -r ./SlickCold/gtk-3.0/internal
-rm -r ./SlickCold/gtk-3.0/widgets
-rm -r ./SlickCold/gtk-3.0/apps
-rm ./SlickCold/gtk-3.0/*.scss
-rm -r ./SlickFire/gtk-3.0/internal
-rm -r ./SlickFire/gtk-3.0/widgets
-rm -r ./SlickFire/gtk-3.0/apps
-rm ./SlickFire/gtk-3.0/*.scss
+rm -r "./SlickCold/gtk-3.0/internal"
+rm -r "./SlickCold/gtk-3.0/widgets"
+rm -r "./SlickCold/gtk-3.0/apps"
+rm "./SlickCold/gtk-3.0/*.scss"
+rm -r "./SlickFire/gtk-3.0/internal"
+rm -r "./SlickFire/gtk-3.0/widgets"
+rm -r "./SlickFire/gtk-3.0/apps"
+rm "./SlickFire/gtk-3.0/*.scss"
 
 echo "Done."
